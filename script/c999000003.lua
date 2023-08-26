78e8003999a9d4bbecd01980e8b289d2b26a674a
BuraiLaplayce <yuri.scb@hotmail.com>
2023-08-26 16:05:51 -0300

    Create c999000003.lua

---
+++ script/c999000003.lua
@@ -0,0 +1,33 @@
+--Red-Eyes Darkness Fusion
+--Scripted by Burai
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Fusion.CreateSummonEff(c,s.ffilter,nil,s.fextra,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,s.extratg)
+	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	c:RegisterEffect(e1)
+end
+s.listed_series={0x3b}
+s.listed_names={id,CARD_REDEYES_B_DRAGON}
+function s.ffilter(c)
+	return c:IsAttribute(ATTRIBUTE_DARK) and c:IsRace(RACE_DRAGON)
+end
+function s.fefilter(c)
+	return c:IsFaceup() and c:IsCode(CARD_REDEYES_B_DRAGON)
+end
+function s.exfilter(c)
+	return c:IsMonster() and c:IsAttribute(ATTRIBUTE_DARK) and c:IsAbleToGrave()
+end
+function s.fextra(e,tp,mg) --duel matching need to lock on red eyes
+	if Duel.IsExistingMatchingCard(s.fefilter,tp,LOCATION_ONFIELD,0,1,nil) then
+		local eg=Duel.GetMatchingGroup(s.exfilter,tp,LOCATION_DECK,0,nil)
+		if eg and #eg>0 then
+			return eg,s.fcheck
+		end
+	end
+	return nil
+end
+function s.extratg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetPossibleOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_DECK)
+end
