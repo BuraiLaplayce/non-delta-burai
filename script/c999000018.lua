7a4af3d98c10d3c7c4fda8b5f5ed1d61d82a732e
BuraiLaplayce <yuri.scb@hotmail.com>
2023-09-25 17:14:57 -0300

    Create c999000018.lua

---
+++ script/c999000018.lua
@@ -0,0 +1,43 @@
+--Evilswarm Yidhra
+--Scripted by Burai
+local s,id=GetID()
+function s.initial_effect(c)
+	--Change 1 monster to face-up or face-down Defense Position
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_POSITION)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_FLIP+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetCost(s.poscost)
+	e1:SetTarget(s.postg)
+	e1:SetOperation(s.posop)
+	c:RegisterEffect(e1)
+end
+s.listed_series={0xa}
+function s.poscfilter(c)
+	return c:IsSetCard(0xa) and c:IsMonster() and c:IsAbleToGraveAsCost()
+end
+function s.poscost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.poscfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectMatchingCard(tp,s.spcfilter,tp,LOCATION_DECK,0,1,1,nil)
+	Duel.SendtoGrave(g,REASON_COST)
+end
+function s.cgfilter(c)
+	return c:IsMonster() and c:IsCanTurnSet()
+end
+function s.postg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return Duel.IsExistingMatchingCard(s.cgfilter,tp,LOCATION_MZONE,0,1,nil) and chk~=c end
+	Duel.SetOperationInfo(0,CATEGORY_POSITION,nil,1,0,0)
+end
+function s.posop(e,tp,eg,ep,ev,re,r,rp,chk)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_POSCHANGE)
+	local g=Duel.SelectMatchingCard(tp,s.cgfilter,tp,LOCATION_MZONE,0,1,1,nil)
+	local tc=g:GetFirst()
+	if tc then
+		local pos=Duel.SelectPosition(tp,tc,POS_FACEUP_DEFENSE|POS_FACEDOWN_DEFENSE)
+		Duel.ChangePosition(tc,pos)
+	end
+end
