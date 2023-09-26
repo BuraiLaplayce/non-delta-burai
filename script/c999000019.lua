e28e408640340881ea10c5b6f24c6ab42ea47077
BuraiLaplayce <yuri.scb@hotmail.com>
2023-09-25 21:51:37 -0300

    Create c999000019.lua

---
+++ script/c999000019.lua
@@ -0,0 +1,35 @@
+--Infestation Manifestation
+--Scripted by Burai
+local s,id=GetID()
+function s.initial_effect(c)
+	c:SetUniqueOnField(1,0,id)
+	--Add 1 "lswarm" monster
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+	--Treate as "lswarm"
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetTargetRange(LOCATION_MZONE,0)
+	e2:SetCode(EFFECT_ADD_SETCODE)
+	e2:SetValue(0xa)
+	c:RegisterEffect(e2)
+end
+s.listed_series={0xa}
+function s.thfilter(c)
+	return c:IsSetCard(0xa) and c:IsAbleToHand()
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	local g=Duel.GetMatchingGroup(s.thfilter,tp,LOCATION_DECK,0,nil)
+	if #g>0 and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local sg=g:Select(tp,1,1,nil)
+		Duel.SendtoHand(sg,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,sg)
+	end
+end
