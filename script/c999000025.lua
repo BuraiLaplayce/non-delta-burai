32a02b9fcf68afb7d13c23e8a32af25366275eaa
BuraiLaplayce <yuri.scb@hotmail.com>
2024-07-09 13:49:51 -0300

    Update Ursarctic First Charge

--- script/c999000025.lua
+++ script/c999000025.lua
@@ -11,6 +11,17 @@ function s.initial_effect(c)
 	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
+	--Tribute Substitute
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,3))
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(CARD_URSARCTIC_BIG_DIPPER)
+	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetTargetRange(1,0)
+	e2:SetCountLimit(1)
+	e2:SetTarget(s.reptg)
+	c:RegisterEffect(e2)
 end
 function s.spfilter(c,e,tp)
 	return c:IsSetCard(0x165) and c:IsMonster() 
@@ -44,3 +55,32 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 		Duel.RegisterEffect(e1,tp)
 	end
 end
+function s.reptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return not c:IsReason(REASON_REPLACE+REASON_COST) 
+		and Duel.CheckLPCost(tp,700) end
+	if Duel.SelectEffectYesNo(tp,aux.Stringid(id,3)) then
+		Duel.PayLPCost(tp,700)
+		return true
+	else return false end
+end
+--[[
+function s.repcfilter(c,extracon,base,params)
+	return c:IsSetCard(SET_URSARCTIC) and c:IsLevelAbove(7) and c:IsAbleToRemoveAsCost()
+		and (not extracon or extracon(base,c,table.unpack(params)))
+end
+function s.repcon(e)
+	return Duel.IsExistingMatchingCard(s.repcfilter,e:GetHandlerPlayer(),LOCATION_GRAVE,0,1,nil)
+end
+function s.repval(base,e,tp,eg,ep,ev,re,r,rp,chk,extracon)
+	local c=e:GetHandler()
+	return c:IsMonster() and c:IsSetCard(SET_URSARCTIC) and
+		(not extracon or Duel.IsExistingMatchingCard(s.repcfilter,e:GetHandlerPlayer(),LOCATION_GRAVE,0,1,nil,extracon,base,{e,tp,eg,ep,ev,re,r,rp,chk}))
+end
+function s.repop(base,e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_CARD,0,id)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g=Duel.SelectMatchingCard(tp,s.repcfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+	Duel.Remove(g,POS_FACEUP,REASON_COST+REASON_REPLACE)
+end
+--]]
