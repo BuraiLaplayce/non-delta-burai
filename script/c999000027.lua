c29c5d2b97d8a7fd5ca066d13c5420200e0db8cf
BuraiLaplayce <yuri.scb@hotmail.com>
2024-07-14 22:41:19 -0300

    Update c999000027.lua

--- script/c999000027.lua
+++ script/c999000027.lua
@@ -5,7 +5,16 @@ function s.initial_effect(c)
 	--Link summon method
 	c:EnableReviveLimit()
 	Link.AddProcedure(c,s.matfilter,1,1)
-		--Activate 1 "Ursarctic Big Dipper" from your Deck
+		--Cannot be targeted
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCondition(function(e) return e:GetHandler():IsLinked() end)
+	e1:SetValue(1)
+	c:RegisterEffect(e1)
+		--Activate 1 "Orcustrated Babel" from your Deck
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,0))
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
@@ -16,6 +25,22 @@ function s.initial_effect(c)
 	e2:SetCondition(s.accon)
 	e2:SetOperation(s.acop)
 	c:RegisterEffect(e2)
+		--Special Summon 1 "Orcust"
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e3:SetType(EFFECT_TYPE_IGNITION)
+	e3:SetCountLimit(1,{id,1})
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCondition(s.spcon1)
+	e3:SetTarget(s.sptg)
+	e3:SetOperation(s.spop)
+	c:RegisterEffect(e3)
+	local e4=e3:Clone()
+	e4:SetType(EFFECT_TYPE_QUICK_O)
+	e4:SetCode(EVENT_FREE_CHAIN)
+	e4:SetCondition(s.spcon2)
+	c:RegisterEffect(e4)
 end
 s.listed_series={SET_ORCUST}
 s.listed_names={90351981}
@@ -39,4 +64,26 @@ function s.acop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOFIELD)
 	local tc=Duel.SelectMatchingCard(tp,s.fieldfilter,tp,LOCATION_DECK,0,1,1,nil,tp):GetFirst()
 	Duel.ActivateFieldSpell(tc,e,tp,eg,ep,ev,re,r,rp)
+end
+function s.spcon1()
+	return Duel.IsMainPhase() and not Duel.IsPlayerAffectedByEffect(tp,CARD_ORCUSTRATED_BABEL)
+end
+function s.spcon2()
+	return Duel.IsMainPhase() and Duel.IsPlayerAffectedByEffect(tp,CARD_ORCUSTRATED_BABEL)
+end
+function s.spfilter(c,e,tp)
+	return c:IsFaceup() and c:IsSetCard(SET_ORCUST) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_GRAVE|LOCATION_REMOVED,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE|LOCATION_REMOVED)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.spfilter),tp,LOCATION_GRAVE|LOCATION_REMOVED,0,1,1,nil,e,tp)
+	if #g>0 then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	end
 end
